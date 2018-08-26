5fe5deb0f23f1578d838c7f18c73c68a960daaf5
Krishna Sagar R <ragas.sagar.474@gmail.com>
2018-08-27 00:04:40 +0100

    remove dash graph

--- dashserver.py
+++ dashserver.py
@@ -5,24 +5,11 @@ import dash_html_components as html
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H3(children='Points'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montral'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
+        Tree
+    ''')
 ])
 
 if __name__ == '__main__':
