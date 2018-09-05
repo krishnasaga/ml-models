f1f4835784a016a357af91f7fdcb1b4efdfd38f5
krishnasagar.r <krishnasagar.r@sonata-software.com>
2018-09-05 21:34:38 +0100

    add cv2

---
+++ cv2.py
@@ -0,0 +1,17 @@
+import cv2 as opencv
+from cv import RectangleDetector
+
+scriptName = sys.argv[0]
+inputdir = sys.argv[1]
+commithash = sys.argv[2]
+outputdir = sys.argv[3]
+
+image = opencv.imread(inputdir+'/flight-options.png')
+
+rectangleDetector = RectangleDetector()
+rectangles,thresh = rectangleDetector.detect(image);
+
+outputimage = opencv.drawContours(image, rectangles, -1, (0,255,0), 3)
+
+opencv.imwrite(outputdir + '/1.png',outputimage)
+opencv.imwrite(outputdir + '/2.png',thresh)
