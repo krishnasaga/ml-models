258dfb20365fd516fc17b0f22e89ebb73dbc1a8c
siddalingana <Siddalingana.Patil@tui.co.uk>
2018-10-07 17:43:51 +0100

    Added feature diff functionality

--- data/FeatureDiff.py
+++ data/FeatureDiff.py
@@ -1,4 +1,12 @@
 import FeatureAccesser
 
+import json
+from pprint import pprint
+from deepdiff import DeepDiff
 
-#Get git repo from configuration path
+with open('./cvio/input/file.txt') as first:
+  firstData = json.load(first)    
+with open('./cvio/input/file1.txt') as second: 
+   secondData = json.load(second)
+
+print (DeepDiff(firstData, secondData, exclude_paths={"root['match']"}))
